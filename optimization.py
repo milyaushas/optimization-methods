97fc1aafd739bf0895fb915bc775e2e11ba1585e
Milyausha Sabirova <57729595+milyaushas@users.noreply.github.com>
2023-06-04 23:16:29 +0300

    upload report and other files

---
+++ optimization.py
@@ -0,0 +1,286 @@
+import numpy as np
+from collections import defaultdict, deque  # Use this for effective implementation of L-BFGS
+from utils import get_line_search_tool
+from datetime import datetime
+
+
+def conjugate_gradients(matvec, b, x_0, tolerance=1e-4, max_iter=None, trace=False, display=False):
+    """
+    Solves system Ax=b using Conjugate Gradients method.
+
+    Parameters
+    ----------
+    matvec : function
+        Implement matrix-vector product of matrix A and arbitrary vector x
+    b : 1-dimensional np.array
+        Vector b for the system.
+    x_0 : 1-dimensional np.array
+        Starting point of the algorithm
+    tolerance : float
+        Epsilon value for stopping criterion.
+        Stop optimization procedure and return x_k when:
+         ||Ax_k - b||_2 <= tolerance * ||b||_2
+    max_iter : int, or None
+        Maximum number of iterations. if max_iter=None, set max_iter to n, where n is
+        the dimension of the space
+    trace : bool
+        If True, the progress information is appended into history dictionary during training.
+        Otherwise None is returned instead of history.
+    display:  bool
+        If True, debug information is displayed during optimization.
+        Printing format is up to a student and is not checked in any way.
+
+    Returns
+    -------
+    x_star : np.array
+        The point found by the optimization procedure
+    message : string
+        'success' or the description of error:
+            - 'iterations_exceeded': if after max_iter iterations of the method x_k still doesn't satisfy
+                the stopping criterion.
+    history : dictionary of lists or None
+        Dictionary containing the progress information or None if trace=False.
+        Dictionary has to be organized as follows:
+            - history['time'] : list of floats, containing time in seconds passed from the start of the method
+            - history['residual_norm'] : list of values Euclidian norms ||g(x_k)|| of the gradient on every step of the algorithm
+            - history['x'] : list of np.arrays, containing the trajectory of the algorithm. ONLY STORE IF x.size <= 2
+    """
+    history = defaultdict(list) if trace else None
+    x_k = np.copy(x_0)
+    # TODO: Implement Conjugate Gradients method.
+
+    result_message = 'success'
+
+    if max_iter is None:
+        max_iter = len(x_0)
+
+    start_time = datetime.now()
+
+    iter = 0
+    Ax_k = matvec(x_k)
+    g_k = Ax_k - b
+    d_k = -g_k
+
+    while iter < max_iter:
+        iter += 1
+
+        Ad_k = matvec(d_k)
+        alpha_k = np.dot(g_k, g_k) / np.dot(d_k, Ad_k)
+        x_k += alpha_k * d_k
+        g_k_1 = g_k + alpha_k * Ad_k
+
+        #if np.linalg.norm(g_k_1, ord=2)**2 <= tolerance:
+            #break
+        if np.linalg.norm(matvec(x_k) - b) <= tolerance * np.linalg.norm(b):
+            break
+
+        beta_k = np.dot(g_k_1, g_k_1) / np.dot(g_k, g_k)
+        d_k = (-1) * g_k_1 + beta_k * d_k
+        g_k = g_k_1
+
+        if trace:
+            history['time'].append(datetime.now() - start_time)
+            history['residual_norm'].append(np.linalg.norm(g_k_1, ord=2))
+            if x_k.size <= 2:
+                history['x'].append(x_k)
+
+    if np.linalg.norm(matvec(x_k) - b) > tolerance * np.linalg.norm(b):
+        result_message = 'iterations_exceeded'
+
+    if display:
+        print(result_message)
+
+    return x_k, result_message, history
+
+
+def lbfgs(oracle, x_0, tolerance=1e-4, max_iter=500, memory_size=10,
+          line_search_options=None, display=False, trace=False):
+    """
+    Limited-memory Broyden–Fletcher–Goldfarb–Shanno's method for optimization.
+
+    Parameters
+    ----------
+    oracle : BaseSmoothOracle-descendant object
+        Oracle with .func() and .grad() methods implemented for computing
+        function value and its gradient respectively.
+    x_0 : 1-dimensional np.array
+        Starting point of the algorithm
+    tolerance : float
+        Epsilon value for stopping criterion.
+    max_iter : int
+        Maximum number of iterations.
+    memory_size : int
+        The length of directions history in L-BFGS method.
+    line_search_options : dict, LineSearchTool or None
+        Dictionary with line search options. See LineSearchTool class for details.
+    display : bool
+        If True, debug information is displayed during optimization.
+        Printing format is up to a student and is not checked in any way.
+    trace:  bool
+        If True, the progress information is appended into history dictionary during training.
+        Otherwise None is returned instead of history.
+
+    Returns
+    -------
+    x_star : np.array
+        The point found by the optimization procedure
+    message : string
+        'success' or the description of error:
+            - 'iterations_exceeded': if after max_iter iterations of the method x_k still doesn't satisfy
+              the stopping criterion.
+    history : dictionary of lists or None
+        Dictionary containing the progress information or None if trace=False.
+        Dictionary has to be organized as follows:
+            - history['func'] : list of function values f(x_k) on every step of the algorithm
+            - history['time'] : list of floats, containing time in seconds passed from the start of the method
+            - history['grad_norm'] : list of values Euclidian norms ||g(x_k)|| of the gradient on every step of the algorithm
+            - history['x'] : list of np.arrays, containing the trajectory of the algorithm. ONLY STORE IF x.size <= 2
+    """
+    history = defaultdict(list) if trace else None
+    line_search_tool = get_line_search_tool(line_search_options)
+    x_k = np.copy(x_0)
+
+    # TODO: Implement L-BFGS method.
+    # Use line_search_tool.line_search() for adaptive step size.
+    return x_k, 'success', history
+
+
+def hessian_free_newton(oracle, x_0, tolerance=1e-4, max_iter=500, 
+                        line_search_options=None, display=False, trace=False):
+    """
+    Hessian Free method for optimization.
+
+    Parameters
+    ----------
+    oracle : BaseSmoothOracle-descendant object
+        Oracle with .func(), .grad() and .hess_vec() methods implemented for computing
+        function value, its gradient and matrix product of the Hessian times vector respectively.
+    x_0 : 1-dimensional np.array
+        Starting point of the algorithm
+    tolerance : float
+        Epsilon value for stopping criterion.
+    max_iter : int
+        Maximum number of iterations.
+    line_search_options : dict, LineSearchTool or None
+        Dictionary with line search options. See LineSearchTool class for details.
+    display : bool
+        If True, debug information is displayed during optimization.
+        Printing format is up to a student and is not checked in any way.
+    trace:  bool
+        If True, the progress information is appended into history dictionary during training.
+        Otherwise None is returned instead of history.
+
+    Returns
+    -------
+    x_star : np.array
+        The point found by the optimization procedure
+    message : string
+        'success' or the description of error:
+            - 'iterations_exceeded': if after max_iter iterations of the method x_k still doesn't satisfy
+              the stopping criterion.
+    history : dictionary of lists or None
+        Dictionary containing the progress information or None if trace=False.
+        Dictionary has to be organized as follows:
+            - history['func'] : list of function values f(x_k) on every step of the algorithm
+            - history['time'] : list of floats, containing time in seconds passed from the start of the method
+            - history['grad_norm'] : list of values Euclidian norms ||g(x_k)|| of the gradient on every step of the algorithm
+            - history['x'] : list of np.arrays, containing the trajectory of the algorithm. ONLY STORE IF x.size <= 2
+    """
+    history = defaultdict(list) if trace else None
+    line_search_tool = get_line_search_tool(line_search_options)
+    x_k = np.copy(x_0)
+
+    # TODO: Implement hessian-free Newton's method.
+    # Use line_search_tool.line_search() for adaptive step size.
+    return x_k, 'success', history
+
+
+def gradient_descent(oracle, x_0, tolerance=1e-5, max_iter=10000,
+                     line_search_options=None, trace=False, display=False):
+    """
+    Gradient descent optimization method.
+
+    Parameters
+    ----------
+    oracle : BaseSmoothOracle-descendant object
+        Oracle with .func(), .grad() and .hess() methods implemented for computing
+        function value, its gradient and Hessian respectively.
+    x_0 : np.array
+        Starting point for optimization algorithm
+    tolerance : float
+        Epsilon value for stopping criterion.
+    max_iter : int
+        Maximum number of iterations.
+    line_search_options : dict, LineSearchTool or None
+        Dictionary with line search options. See LineSearchTool class for details.
+    trace : bool
+        If True, the progress information is appended into history dictionary during training.
+        Otherwise None is returned instead of history.
+    display : bool
+        If True, debug information is displayed during optimization.
+        Printing format and is up to a student and is not checked in any way.
+
+    Returns
+    -------
+    x_star : np.array
+        The point found by the optimization procedure
+    message : string
+        "success" or the description of error:
+            - 'iterations_exceeded': if after max_iter iterations of the method x_k still doesn't satisfy
+                the stopping criterion.
+            - 'computational_error': in case of getting Infinity or None value during the computations.
+    history : dictionary of lists or None
+        Dictionary containing the progress information or None if trace=False.
+        Dictionary has to be organized as follows:
+            - history['time'] : list of floats, containing time in seconds passed from the start of the method
+            - history['func'] : list of function values f(x_k) on every step of the algorithm
+            - history['grad_norm'] : list of values Euclidian norms ||g(x_k)|| of the gradient on every step of the algorithm
+            - history['x'] : list of np.arrays, containing the trajectory of the algorithm. ONLY STORE IF x.size <= 2
+
+    Example:
+    --------
+    >> oracle = QuadraticOracle(np.eye(5), np.arange(5))
+    >> x_opt, message, history = gradient_descent(oracle, np.zeros(5), line_search_options={'method': 'Armijo', 'c1': 1e-4})
+    >> print('Found optimal point: {}'.format(x_opt))
+       Found optimal point: [ 0.  1.  2.  3.  4.]
+    """
+    history = defaultdict(list) if trace else None
+    line_search_tool = get_line_search_tool(line_search_options)
+    x_k = np.copy(x_0)
+
+    # TODO: Implement gradient descent
+    # Use line_search_tool.line_search() for adaptive step size.er:
+    start_time = datetime.now()
+    it = 0
+    alpha = None
+    result_message = "success"
+
+    try:
+        while it < max_iter and np.linalg.norm(oracle.grad(x_k), ord=2)**2 > tolerance * (np.linalg.norm(oracle.grad(x_0), ord=2)**2):
+            d_k = -oracle.grad(x_k)
+            alpha = line_search_tool.line_search(oracle, x_k, d_k, alpha)
+            if alpha is None:
+                result_message = 'computational_error'
+                break
+            x_k += float(alpha) * d_k
+            if trace:
+                history['time'].append(datetime.now() - start_time)
+                history['func'].append(oracle.func(x_k))
+                history['grad_norm'].append(np.linalg.norm(oracle.grad(x_k), ord=2))
+                if len(x_k) <= 2:
+                    history['x'].append(x_k)
+            #if display:
+                #print(f"iter={it}, x_k={x_k}, f(x_k)={oracle.func(x_k)}")
+            it += 1
+    except Exception as e:
+        if display:
+            print(str(e))
+        result_message = 'computational_error'
+
+    if it == max_iter and np.linalg.norm(oracle.grad(x_k))**2 > tolerance * (np.linalg.norm(oracle.grad(x_0)) ** 2):
+        result_message = 'iterations_exceeded'
+
+    if display:
+        print(result_message)
+
+    return x_k, result_message, history
